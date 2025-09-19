bdf109d71424a3fe3138ee91bd1210ae2e86a440
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-09-19 15:03:40 +0300

    Add project files.

---
+++ Assignment 2 Linq/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment_2_Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
