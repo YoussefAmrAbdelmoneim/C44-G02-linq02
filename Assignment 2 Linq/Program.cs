bb278d766dea4f94d360ae5c42b75016e8766b7c
YoussefAmrAbdelmoneim <yosefamr200@gmail.com>
2025-09-19 15:10:27 +0300

    Element Operators

--- Assignment 2 Linq/Program.cs
+++ Assignment 2 Linq/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace Assignment_2_Linq
+﻿using Demo.Data;
+namespace Assignment_2_Linq
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Element Operators
+            //var result = ListGenerator.ProductList?.FirstOrDefault();
+            //result = ListGenerator.ProductList?.FirstOrDefault(p => p.UnitPrice > 1000);
+            //int[] Arr = { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 };
+            //var result02 = Arr.Where(n=>n>5).Skip(1).FirstOrDefault();
+            #endregion
         }
     }
 }
